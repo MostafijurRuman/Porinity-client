5e99f85fc413bffcf1c1870f6bea269fb8458c03
MostafijurRuman <mustafizur.ruman7@gmail.com>
2025-09-18 13:31:02 +0600

    chore(client): init react app + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-black bg-bg-light text-primary text-center py-5">
+        Hello World
+      </h1>
+      <h1 className=" font-bold text-7xl  bg-bg-light text-primary-accent font-bn text-center py-5">
+        বৃহত্তম ইসলামিক ম্যাট্রিমনি
+      </h1>
+    </>
+  );
+}
+
+export default App;
